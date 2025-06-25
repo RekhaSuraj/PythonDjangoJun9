36db2b2a730d33e479bab968e08ee2c75e929006
rekha <rekhakirani@gmail.com>
2025-06-25 12:45:27 +0530

    'SessionMgmt'

--- project8/app1/views.py
+++ project8/app1/views.py
@@ -5,9 +5,20 @@ from django.shortcuts import render
 
 def count_page(request):
     print("current cookies:",request.COOKIES)
+    # Logs all cookies from the current request to the console (for debugging).
+
     count = int(request.COOKIES.get('count',0))
+    # Retrieves the cookie named 'count'. If it doesn’t exist, defaults to 0.
+
     count = count + 1
+    # Increments the count by 1.
 
     response = render(request,'count.html',{'count':count})
+    # Renders the count.html template, passing the current count.
+
     response.set_cookie('count',count)
+    # Sets or updates the 'count' cookie with the new value.
+
     return response
+    #Returns the HTTP response with the updated cookie.
+
